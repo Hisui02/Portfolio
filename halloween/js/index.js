ca75156044a8e45376ec36cadbb14cd9dd3a12d1
Hisui <alvarolgleiva@gmail.com>
2023-11-17 14:40:37 +0100

    done

--- halloween/js/index.js
+++ halloween/js/index.js
@@ -5,3 +5,12 @@ const show = (id) => {
 const hide = (id) => {
   document.getElementById(id).style.visibility = "hidden";
 };
+
+const sound = (id) => {
+  document.getElementById(id).play();
+};
+
+const silence = (id) => {
+  document.getElementById(id).currentTime = 0;
+  document.getElementById(id).pause();
+};
